6d573055d7d419dadcbbf06efde96189044c3cb8
Daniel Olanrewaju <danorelanre@gmail.com>
2023-06-22 00:34:58 +0100

    feat: create user request

---
+++ app/Requests/UserRequests.go
@@ -0,0 +1,10 @@
+package Requests
+
+type CreateUserRequest struct {
+	FirstName   string `json:"firstName" binding:"required"`
+	LastName    string `json:"lastName" binding:"required"`
+	UserName    string `json:"userName" binding:"required"`
+	Email       string `json:"email" binding:"required"`
+	PhoneNumber string `json:"phoneNumber" binding:"required"`
+	Password    string `json:"password" binding:"required"`
+}
