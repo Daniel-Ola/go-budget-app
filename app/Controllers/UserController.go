bd11d6de2f0cbffc6d7705f547b190e975c4e126
Daniel Olanrewaju <danorelanre@gmail.com>
2023-06-29 14:59:23 +0100

    refactor: return user details from user endpoint

--- app/Controllers/UserController.go
+++ app/Controllers/UserController.go
@@ -1,7 +1,24 @@
 package Controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"nielscript.com/budgetapp/api/app/Models"
+	UserServices "nielscript.com/budgetapp/api/app/Services"
+)
 
 func User(context *gin.Context) {
+
+	//var user Models.User
+
+	claims, _ := UserServices.GetTokenClaims(context)
+
+	user, err := Models.FindUserByField("user_name", claims.Username)
+
+	if err != nil {
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "User info returned", "data": user})
 
 }
